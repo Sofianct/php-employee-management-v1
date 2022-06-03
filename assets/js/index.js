bfb5530a22d265b7988a1943f39d224c46e62471
Sofia Romera <sofia-romera@outlook.com>
2022-06-03 11:14:43 +0200

    Add employee form

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,146 +4,214 @@ const deleteButtons = document.querySelectorAll("[data-delete]");
 const urlController = "./library/employeeController.php";
 const urlControllerGet = "./library/employeeController.php?id=";
 
+window.onload = async () => {
 
-getEmployees();
+    await getEmployees();
 
-//display employees table
-async function getEmployees() {
-    const res = await fetch(urlController, {
-        method: 'GET',
-        headers: {
-            "content-type": "application/json",
+    initializeBtn();
+
+    //display employees table
+    async function getEmployees() {
+        const res = await fetch(urlController, {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+            }
+        });
+        const data = await res.json();
+        //console.log(data);
+        data.forEach(employee => {
+            const tbody = document.getElementById('tbody');
+            const tr = document.createElement('tr');
+            tr.dataset.row = employee.id;
+            const imgtd = document.createElement('td');
+            const img = document.createElement('img');
+            const name = document.createElement('td');
+            const email = document.createElement('td');
+            const age = document.createElement('td');
+            const streetNo = document.createElement('td');
+            const city = document.createElement('td');
+            const state = document.createElement('td');
+            const posCode = document.createElement('td');
+            const phNumber = document.createElement('td');
+            const btns = document.createElement('td');
+            const editBtn = document.createElement('button');
+            const deleteBtn = document.createElement('button');
+            const editIcon = document.createElement('i');
+            const deleteIcon = document.createElement('i');
+
+            editIcon.classList.add('fa-solid', 'fa-pen-to-square', 'link-dark');
+            deleteIcon.classList.add('fa-solid', 'fa-trash-can', 'link-dark');
+            editBtn.classList.add('btn', 'btn-link');
+            deleteBtn.classList.add('btn', 'btn-link');
+            editBtn.appendChild(editIcon);
+            deleteBtn.appendChild(deleteIcon);
+            btns.append(editBtn, deleteBtn);
+
+            img.setAttribute('src', 'https://mdbootstrap.com/img/new/avatars/8.jpg');
+            img.classList.add('rounded-circle');
+            img.style.width = "45px";
+            img.style.height = "45px";
+            imgtd.appendChild(img);
+
+            name.textContent = employee.name + ' ' + employee.lastName;
+            email.textContent = employee.email;
+            age.textContent = employee.age;
+            streetNo.textContent = employee.streetAddress;
+            city.textContent = employee.city;
+            state.textContent = employee.state;
+            posCode.textContent = employee.postalCode;
+            phNumber.textContent = employee.phoneNumber;
+
+            tr.append(imgtd, name, email, age, streetNo, city, state, posCode, phNumber, btns);
+            tbody.appendChild(tr);
+            console.log(tbody);
+            //add event listener to go to employee.php and show the selected employee.
+
+
+        });
+    }
+
+    function initializeBtn() {
+        const rowBtn = document.querySelectorAll('[data-row]');
+
+        Array.from(rowBtn).map(btn => {
+            btn.addEventListener('click', (e) => {
+                let id = e.target.parentElement.getAttribute('data-row');
+                console.log(id);
+                showEmployee(id);
+                //window.location = "../src/employee.php";
+            });
+        });
+        console.log(rowBtn);
+    }
+
+
+
+
+
+    //Add edit event listener to all edit buttons
+    Array.from(editButtons).map(btn => {
+        btn.addEventListener('click', async (e) => {
+            const employeeId = e.currentTarget.getAttribute('data-id'); //get ID
+            const employee = await getEmployee(employeeId); //get data employee
+            showEditRow(employee, employeeId); //show edit form row
+        });
+    });
+
+    //Add delete event listener to all delete buttons
+    Array.from(deleteButtons).map(btn => {
+        btn.addEventListener('click', (e) => {
+            const employeeId = e.currentTarget.getAttribute('data-id');
+            deleteEmployee(urlManagerController, employeeId);
+        });
+    });
+
+    //fetch to edit employee
+    async function editEmployee(url, id) {
+
+        const response = await fetch(url, {
+            method: "PUT",
+            body: id,
+        });
+
+        const data = await response.text();
+        console.log(data);
+    }
+
+    //fetch to delete employee
+    async function deleteEmployee(url, id) {
+
+        const response = await fetch(url, {
+            method: "DELETE",
+            body: id,
+        });
+
+        const data = await response.text();
+        console.log(data);
+    }
+
+    //get data employee
+    async function getEmployee(id) {
+
+        const response = await fetch(`${urlControllerGet}${id}`, {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        return data;
+
+    }
+
+    //show edit form in row target
+    function showEditRow(employee, id) {
+        const idRow = `row${id}`;
+        const employeeRow = document.getElementById(idRow);
+
+        //get all data from tr
+        const employeeDataSelector = employeeRow.querySelectorAll('[data-td]');
+
+        //for each td insert input form
+        employeeDataSelector[0].textContent = `<td><form action="" method="post" id="editFormRow"></form>`;
+        Array.from(employeeDataSelector).slice(1).map((td) => {
+
+        })
+        `< input value = ${employee.name} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.email} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.age} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.city} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.state} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.postalCode} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.phoneNumber} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+        `< input value = ${employee.name} form="editFormRow" type="text" name="name" class="form-control" required></td>`
+
+    }
+
+    async function showEmployee(id) {
+        const response = await fetch(`${urlControllerGet}${id}`, {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+
+        const name = document.getElementById('name');
+        const lastName = document.getElementById('lastName');
+        const email = document.getElementById('email');
+        const genderSelect = document.getElementById('gender');
+        const city = document.getElementById('city');
+        const streetAddress = document.getElementById('streetAddress');
+        const state = document.getElementById('state');
+        const age = document.getElementById('age');
+        const postalCode = document.getElementById('postalCode');
+        const phoneNumber = document.getElementById('phoneNumber');
+
+        name.setAttribute('value', data.name);
+        lastName.setAttribute('value', data.lastName);
+        email.setAttribute('value', data.email);
+        city.setAttribute('value', data.city);
+        streetAddress.setAttribute('value', data.streetAddress);
+        state.setAttribute('value', data.state);
+        age.setAttribute('value', data.age);
+        postalCode.setAttribute('value', data.postalCode);
+        phoneNumber.setAttribute('value', data.phoneNumber);
+
+        if (data.gender == 'woman') {
+            genderSelect.selectedIndex = "0";
+        } else if (data.gender == 'man') {
+            genderSelect.selectedIndex = "1";
+        } else {
+            genderSelect.selectedIndex = "2";
         }
-    });
-    const data = await res.json();
-    console.log(data);
-    data.forEach(employee => {
-        const tbody = document.getElementById('tbody');
-        const tr = document.createElement('tr');
-        const imgtd = document.createElement('td');
-        const img = document.createElement('img');
-        const name = document.createElement('td');
-        const email = document.createElement('td');
-        const age = document.createElement('td');
-        const streetNo = document.createElement('td');
-        const city = document.createElement('td');
-        const state = document.createElement('td');
-        const posCode = document.createElement('td');
-        const phNumber = document.createElement('td');
-        const btns = document.createElement('td');
-        const editBtn = document.createElement('button');
-        const deleteBtn = document.createElement('button');
-        const editIcon = document.createElement('i');
-        const deleteIcon = document.createElement('i');
 
-        editIcon.classList.add('fa-solid', 'fa-pen-to-square', 'link-dark', 'p-2');
-        deleteIcon.classList.add('fa-solid', 'fa-trash-can', 'link-dark');
-        editBtn.classList.add('btn', 'btn-link');
-        deleteBtn.classList.add('btn', 'btn-link');
-        editBtn.appendChild(editIcon);
-        deleteBtn.appendChild(deleteIcon);
-        btns.append(editBtn, deleteBtn);
+        window.addEventListener('load', function () {
+            alert("It's loaded!")
+        })
 
-        img.setAttribute('src', 'https://mdbootstrap.com/img/new/avatars/8.jpg');
-        img.classList.add('rounded-circle');
-        img.style.width = "45px";
-        img.style.height = "45px";
-        imgtd.appendChild(img);
-
-        name.textContent = employee.name + ' ' + employee.lastName;
-        email.textContent = employee.email;
-        age.textContent = employee.age;
-        streetNo.textContent = employee.streetAddress;
-        city.textContent = employee.city;
-        state.textContent = employee.state;
-        posCode.textContent = employee.postalCode;
-        phNumber.textContent = employee.phoneNumber;
-
-        tr.append(imgtd, name, email, age, streetNo, city, state, posCode, phNumber, btns);
-        tbody.append(tr);
-    });
-}
-
-
-//Add edit event listener to all edit buttons
-Array.from(editButtons).map(btn => {
-    btn.addEventListener('click', async (e) => {
-        const employeeId = e.currentTarget.getAttribute('data-id'); //get ID
-        const employee = await getEmployee(employeeId); //get data employee
-        showEditRow(employee, employeeId); //show edit form row
-    });
-});
-
-//Add delete event listener to all delete buttons
-Array.from(deleteButtons).map(btn => {
-    btn.addEventListener('click', (e) => {
-        const employeeId = e.currentTarget.getAttribute('data-id');
-        deleteEmployee(urlManagerController, employeeId);
-    });
-});
-
-//fetch to edit employee
-async function editEmployee(url, id) {
-
-    const response = await fetch(url, {
-        method: "PUT",
-        body: id,
-    });
-
-    const data = await response.text();
-    console.log(data);
-}
-
-//fetch to delete employee
-async function deleteEmployee(url, id) {
-
-    const response = await fetch(url, {
-        method: "DELETE",
-        body: id,
-    });
-
-    const data = await response.text();
-    console.log(data);
-}
-
-//get data employee
-async function getEmployee(id) {
-
-    const response = await fetch(`${urlControllerGet}${id}`, {
-        method: 'GET',
-        headers: {
-            "content-type": "application/json",
-        }
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
-
-}
-
-//show edit form in row target
-function showEditRow(employee, id) {
-    const idRow = `row${id}`;
-    const employeeRow = document.getElementById(idRow);
-
-    //get all data from tr
-    const employeeDataSelector = employeeRow.querySelectorAll('[data-td]');
-
-    //for each td insert input form
-    employeeDataSelector[0].textContent = `<td><form action="" method="post" id="editFormRow"></form>`;
-    Array.from(employeeDataSelector).slice(1).map((td) => {
-
-    })
-    `< input value = ${employee.name} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.email} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.age} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.city} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.state} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.postalCode} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.phoneNumber} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-    `< input value = ${employee.name} form="editFormRow" type="text" name="name" class="form-control" required></td>`
-
-
-
-
+    }
 }
--- src/employee.php
+++ src/employee.php
@@ -1,1 +1,118 @@
-<!-- TODO Employee view -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ae63adffc0.js" crossorigin="anonymous" defer></script>
+    <script src="../assets/js/index.js" defer></script>
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <title>Employee Management</title>
+</head>
+
+<body>
+    <header>
+        <?php include_once("../assets/html/header.html"); ?>
+    </header>
+
+    <main>
+
+        <form>
+            <!-- 2 column grid layout with text inputs -->
+            <div class="row m-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example1">Name</label>
+                        <input type="text" id="name" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Last name</label>
+                        <input type="text" id="lastName" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <!-- 2 column grid layout with text inputs -->
+            <div class="row m-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example1">Email Address</label>
+                        <input type="text" id="email" class="form-control" />
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Gender</label>
+                        <select id="gender" class="form-select" aria-label="Default select example">
+                            <option value="female">Female</option>
+                            <option value="male">Male</option>
+                            <option value="notAnswered">Prefer not to respond</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <!-- 2 column grid layout with text inputs -->
+            <div class="row m-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example1">City</label>
+                        <input type="text" id="city" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Street Address</label>
+                        <input type="text" id="streetAddress" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <!-- 2 column grid layout with text inputs -->
+            <div class="row m-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example1">State</label>
+                        <input type="text" id="state" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Age</label>
+                        <input type="text" id="age" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <!-- 2 column grid layout with text inputs -->
+            <div class="row m-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example1">Postal Code</label>
+                        <input type="text" id="postalCode" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="form3Example2">Phone Number</label>
+                        <input type="text" id="phoneNumber" class="form-control" />
+                    </div>
+                </div>
+            </div>
+
+            <!-- Submit button -->
+            <button type="" class="btn btn-primary btn-block ms-5">Submit</button>
+            <button type="" class="btn btn-secondary btn-block ms-2">Return</button>
+        </form>
+
+    </main>
+
+    <footer>
+        <?php include_once("../assets/html/footer.html"); ?>
+    </footer>
+
+</body>
+
+</html>
