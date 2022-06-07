cc162251532776e9f9d09b56c553388820614550
Alicia Cembranos <alicembru15@gmail.com>
2022-06-07 09:35:48 +0200

    modify array employee POST request

--- src/library/employeeController.php
+++ src/library/employeeController.php
@@ -25,7 +25,7 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
                 'lastName' => "",
                 'email' => "",
                 'gender' => "",
-                'age' => 24,
+                'age' => "",
                 'streetAddress' => "",
                 'city' => "",
                 'state' => "",
